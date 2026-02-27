3e8eabf724b604337f9eee501b80c1999a3be867
csendin <eucaiosendin@gmail.com>
2026-02-27 19:05:28 -0300

    feat: add proxy module and proxy service

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ProxyModule } from './proxy/proxy.module'
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60000, // 1 minute
+                limit: 100, // 100 requests per minute
+            },
+        ]),
+        ProxyModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ api-gateway/src/proxy/proxy.module.ts
@@ -0,0 +1,11 @@
+import { HttpModule } from '@nestjs/axios'
+import { Module } from '@nestjs/common'
+
+import { ProxyService } from './service/proxy.service'
+
+@Module({
+    imports: [HttpModule],
+    providers: [ProxyService],
+    exports: [ProxyService],
+})
+export class ProxyModule {}
